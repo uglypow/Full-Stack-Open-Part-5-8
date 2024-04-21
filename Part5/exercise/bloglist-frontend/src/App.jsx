94de5b26209bde339191b5a38fe37186671c85cb
Puriwat <uglypow@gmail.com>
2024-04-22 02:02:21 +0700

    finished exercise 5.11

--- Part5/exercise/bloglist-frontend/src/App.jsx
+++ Part5/exercise/bloglist-frontend/src/App.jsx
@@ -1,18 +1,19 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
+
 import Blog from './components/Blog'
 import Notification from './components/Notification'
+import BlogForm from './components/BlogForm'
+import LoginForm from './components/LoginForm'
+import Togglable from './components/Togglable'
+
 import blogService from './services/blogs'
 import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [message, setMessage] = useState({ text: null, type: null })
-  const [title, setTitle] = useState('')
-  const [author, setAuthor] = useState('')
-  const [url, setUrl] = useState('')
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
+  const blogFormRef = useRef()
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -29,55 +30,84 @@ const App = () => {
     }
   }, [])
 
-  const addBlog = async (event) => {
-    event.preventDefault()
-    const blogObject = {
-      title: title,
-      author: author,
-      url: url,
-    }
-
-    try {
-      const returnedBlog = await blogService.create(blogObject)
-      setBlogs(blogs.concat(returnedBlog))
-      setTitle('')
-      setAuthor('')
-      setUrl('')
-      setMessage({
-        text: `a new blog ${returnedBlog.title} by ${returnedBlog.author} added`,
-        type: 'success'
+  const addBlog = (blogObject) => {
+    blogService
+      .create(blogObject)
+      .then(returnedBlog => {
+        blogFormRef.current.toggleVisibility()
+        setBlogs(blogs.concat(returnedBlog))
+        setMessage({
+          text: `a new blog ${returnedBlog.title} by ${returnedBlog.author} added`,
+          type: 'success'
+        })
+        setTimeout(() => {
+          setMessage(null)
+        }, 5000)
       })
-      setTimeout(() => {
-        setMessage(null)
-      }, 5000)
-    } catch (exception) {
-    }
+      .catch(error => {
+        setMessage({
+          text: error.response.data.error,
+          type: 'error'
+        })
+        setTimeout(() => {
+          setMessage(null)
+        }, 5000)
+      })
   }
 
-  const handleLogin = async (event) => {
-    event.preventDefault()
-
-    try {
-      const user = await loginService.login({
-        username, password,
+  const updateBlog = (id, blogObject) => {
+    blogService
+      .update(id, blogObject)
+      .then(returnedBlog => {
+        setBlogs(blogs.map(blog => blog.id !== id ? blog : returnedBlog))
       })
-
-      window.localStorage.setItem(
-        'loggedNoteappUser', JSON.stringify(user)
-      )
-      blogService.setToken(user.token)
-      setUser(user)
-      setUsername('')
-      setPassword('')
-    } catch (exception) {
-      setMessage({
-        text: 'wrong username or password',
-        type: 'error'
+      .catch(error => {
+        setMessage({
+          text: error.response.data.error,
+          type: 'error'
+        })
+        setTimeout(() => {
+          setMessage(null)
+        }, 5000)
       })
-      setTimeout(() => {
-        setMessage(null)
-      }, 5000)
-    }
+  }
+
+  const removeBlog = (id) => {
+    blogService
+      .remove(id)
+      .then(() => {
+        setBlogs(blogs.filter(blog => blog.id !== id))
+      })
+      .catch(error => {
+        setMessage({
+          text: error.response.data.error,
+          type: 'error'
+        })
+        setTimeout(() => {
+          setMessage(null)
+        }, 5000)
+      })
+  }
+
+  const handleLogin = (userObject) => {
+    loginService
+      .login(userObject)
+      .then(user => {
+        window.localStorage.setItem(
+          'loggedNoteappUser', JSON.stringify(user)
+        )
+        blogService.setToken(user.token)
+        setUser(user)
+      })
+      .catch(error => {
+        setMessage({
+          text: 'wrong username or password',
+          type: 'error'
+        })
+        setTimeout(() => {
+          setMessage(null)
+        }, 5000)
+      })
   }
 
   const handleLogout = () => {
@@ -85,69 +115,14 @@ const App = () => {
     setUser(null)
   }
 
-  const loginForm = () => (
-    <form onSubmit={handleLogin}>
-      <div>
-        username
-        <input
-          type="text"
-          value={username}
-          name="Username"
-          onChange={({ target }) => setUsername(target.value)}
-        />
-      </div>
-      <div>
-        password
-        <input
-          type="password"
-          value={password}
-          name="Password"
-          onChange={({ target }) => setPassword(target.value)}
-        />
-      </div>
-      <button type="submit">login</button>
-    </form>
-  )
-
-  const blogForm = () => (
-    <form onSubmit={addBlog}>
-      <div>
-        title
-        <input
-          type="text"
-          value={title}
-          name="title"
-          onChange={({ target }) => setTitle(target.value)}
-        />
-      </div>
-      <div>
-        author
-        <input
-          type="text"
-          value={author}
-          name="author"
-          onChange={({ target }) => setAuthor(target.value)}
-        />
-      </div>
-      <div>
-        url
-        <input
-          type="text"
-          value={url}
-          name="url"
-          onChange={({ target }) => setUrl(target.value)}
-        />
-      </div>
-      <button type="submit">create</button>
-    </form>
-  )
+  blogs.sort((a, b) => b.likes - a.likes)
 
   return (
     <div>
       <h2>blogs</h2>
       <Notification message={message} />
       {user === null ?
-        loginForm()
+        <LoginForm handleLogin={handleLogin} />
         :
         <div>
           <p>
@@ -155,9 +130,17 @@ const App = () => {
             <button onClick={handleLogout}>logout</button>
           </p>
           <h2>create new</h2>
-          {blogForm()}
+          <Togglable buttonLabel="new blog" ref={blogFormRef} >
+            <BlogForm createBlog={addBlog} />
+          </Togglable>
           {blogs.map(blog =>
-            <Blog key={blog.id} blog={blog} />
+            <Blog
+              key={blog.id}
+              blog={blog}
+              user={user}
+              updateBlog={updateBlog}
+              removeBlog={removeBlog}
+            />
           )}
         </div>
       }
--- Part5/exercise/bloglist-frontend/src/components/Blog.jsx
+++ Part5/exercise/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,58 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog, user, updateBlog, removeBlog }) => {
+  const [showDetail, setShowDetail] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const handleView = () => {
+    setShowDetail(!showDetail)
+  }
+
+  const handleLike = () => {
+    updateBlog(blog.id, { ...blog, likes: blog.likes + 1 })
+  }
+
+  const handleRemove = () => {
+    if (window.confirm(`Remove blog ${blog.title} by ${blog.author}`)) {
+      removeBlog(blog.id)
+    }
+  }
+
+  return (
+    <div style={blogStyle}>
+      {showDetail ?
+        <div>
+          <div>
+            {blog.title} by {blog.author}
+            <button onClick={handleView}>hide</button>
+          </div>
+          <a href={blog.url} target="_blank"><div>{blog.url}</div></a>
+          <div>
+            likes {blog.likes}
+            <button onClick={handleLike}>like</button>
+          </div>
+          <div>{blog.user.name}</div>
+          {user.username === blog.user.username ?
+            <button onClick={handleRemove}>remove</button>
+            :
+            null
+          }
+        </div>
+        :
+        <div>
+          {blog.title} {blog.author}
+          <button onClick={handleView}>view</button>
+        </div>
+      }
+    </div>
+  )
+}
 
 export default Blog
