70e2c67a80879c9a0c698716818a2ef7ca39f28b
Puriwat <uglypow@gmail.com>
2024-04-22 13:51:54 +0700

    finished part 5 'ESlint'

--- Part5/frontend/src/components/LoginForm.jsx
+++ Part5/frontend/src/components/LoginForm.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import PropTypes from 'prop-types'
 
 const LoginForm = ({
     handleLogin,
@@ -36,6 +37,10 @@ const LoginForm = ({
             </form>
         </div>
     )
+}
+
+LoginForm.propTypes = {
+    handleLogin: PropTypes.func.isRequired,
 }
 
 export default LoginForm
